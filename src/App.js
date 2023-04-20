e25f76c5284e6bb2154ff745687b38b79560ed83
Daniel Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2023-04-19 23:33:34 -0300

    se creo pagina de pedido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route} from 'react-router-dom';
+import Producto from './pages/Producto/Producto';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<h1>Home</h1>}></Route>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/producto/:id' element={ <Producto /> }>
     </Routes>
     </>
   );
